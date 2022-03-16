06023216899a82dfe3abea15ed7deb7f55632e98
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-03-16 14:11:21 +0200

    encrypt password and add cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+
+  app.use(cookieSession({
+    keys: ["CARS_PLATFORM"],
+  }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   );
+
   await app.listen(3000, () => console.log('listening to Port 3000'));
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,16 +3,35 @@ import { Controller, Post, Get, Patch, Delete, Param, Query, Body, UseIntercepto
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UserDto } from './dto/user.dto';
 import { UsersService } from "./users.service";
+import { AuthService } from "./auth.service";
 import { Serialize } from './../interceptors/serialize.interceptors';
+import { Session } from "@nestjs/common";
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-    constructor(private userService: UsersService) { }
+    constructor(
+        private userService: UsersService,
+        private authService: AuthService
+    ) { }
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        this.userService.create(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get('/currentuser')
+    currentuser(@Session() session: any) {
+        return this.userService.findOne(session.userId);
     }
 
     @Get('/:id')
